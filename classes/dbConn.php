501a6a778fbeb1a0aefa6268453005f2be4560b8
Dale <yl622@njit.edu>
2017-11-11 16:04:47 -0500

    Fixed the format of dbConn.php

--- classes/dbConn.php
+++ classes/dbConn.php
@@ -1,21 +1,21 @@
 <?php
-class dbConn{
-    protected static $db;
-    private function __construct() {
-        global $connErr;
-        try {
-            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
-            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    class dbConn{
+        protected static $db;
+        private function __construct() {
+            global $connErr;
+            try {
+                self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+                self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            }
+            catch (PDOException $e) {
+                $connErr .= htmlTags::changeRow("Connection Error: " . $e->getMessage());
+            }
         }
-        catch (PDOException $e) {
-            $connErr .= htmlTags::changeRow("Connection Error: " . $e->getMessage());
+        public static function getConnection(&$connErr) {
+            if (!self::$db) {
+                new dbConn();
+            }
+            return self::$db;
         }
     }
-    public static function getConnection(&$connErr) {
-        if (!self::$db) {
-            new dbConn();
-        }
-        return self::$db;
-    }
-}
 ?>
