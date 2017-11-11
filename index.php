c42fdf6d0fc930a46219fcc891a04256036159f9
Dale <yl622@njit.edu>
2017-11-11 15:40:34 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    //Bug report massage
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    //autoload all classes
+    class Manage {
+        static public function autoload($class) {
+            include './classes/' . $class . '.php';
+        }
+    }
+    //register all loaded classes
+    spl_autoload_register(array('Manage', 'autoload'));
+    define('DATABASE', 'yl622');
+    define('USERNAME', 'yl622');
+    define('PASSWORD', 'evPkHDGVf');
+    define('CONNECTION', 'sql2.njit.edu');
+    $obj = new main();
+?>
