7aa46d0ba3b2981a7698d391777c683f3f4171d8
Dale <yl622@njit.edu>
2017-11-11 15:53:10 -0500

    Added htmlTags.php

---
+++ classes/htmlTags.php
@@ -0,0 +1,40 @@
+<?php
+    class htmlTags {
+        static public function changeRow($input) {
+            return $input . '</br>';
+        }
+        static public function htmlHead() {
+            return '<html>';
+        }
+        static public function htmlBody() {
+            return '<body>';
+        }
+        static public function htmlEnd() {
+            return '</html>';
+        }
+        static public function bodyEnd() {
+            return '</body>';
+        }
+        static public function tableHead($name) {
+            return '<table id=' . $name . '>';
+        }
+        static public function tableLineStart() {
+            return '<tr>';
+        }
+        static public function tableLineEnd() {
+            return '</tr>';
+        }
+        static public function tableTitle($input) {
+            return '<th>' . $input . '</th>';
+        }
+        static public function tableDetail($input) {
+            return '<td>' . $input . '</td>';
+        }
+        static public function tableEnd() {
+            return '</table>';
+        }
+        static public function headingOne($input) {
+            return '<h1>' . $input . '</h1>';
+        }
+    }
+?>
