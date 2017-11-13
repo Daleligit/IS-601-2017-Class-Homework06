6fa2eed365392bbbcb0a62d2c8b2b62bf2c51415
Dale <yl622@njit.edu>
2017-11-12 20:42:34 -0500

    Added model.php

---
+++ classes/model.php
@@ -0,0 +1,67 @@
+<?php
+    abstract class model
+    {
+        protected $tableName;
+        public function save()
+        {
+            global $sqlErr;
+            $db = dbConn::getConnection();
+            if (!empty($db)) {
+                $tableName = get_called_class();
+                $array = get_object_vars($this);
+                $columArray = array_keys($array);
+                $columnString = implode(',', $columArray);
+                $valueString = implode(',', $array);
+                if (empty($tableName::findOne($this->id))) {
+                    $sql = $this->insert($tableName, $columnString, $valueString);
+                    $result = htmlTags::changeRow('I just inserted a new record with id = ' . $this->id);
+                } else {
+                    $sql = $this->update($tableName, $columArray, $array);
+                    $result = htmlTags::changeRow('I just updated a record with id = ' . $this->id);
+                }
+                try {
+                    $statement = $db->prepare($sql);
+                    $statement->execute();
+                    return $result;
+                } catch (PDOException $e) {
+                    $sqlErr .= htmlTags::changeRow('SQL query error: ' . $e->getMessage());
+                }
+            }
+        }
+        private function insert($tableName, $columString, $valueString) {
+            $sql = 'INSERT INTO ' . $tableName . ' (' . $columString . ') VALUES (' . $valueString . ')';
+            return $sql;
+        }
+        private function update($tableName, $columArray, $array) {
+            $sql = 'UPDATE ' . $tableName . 'SET ';
+            $temp = 0;
+            foreach ($columArray as $key=>$colum) {
+                if ($colum != 'id') {
+                    if ($temp != 0) {
+                        $sql .= ', ';
+                    }
+                    $sql .= $columArray[$key] . ' = ' . $array[$key];
+                    $temp++;
+                }
+            }
+            $sql .= ' WHERE id = ' . $this->id;
+            return $sql;
+        }
+        public function delete($id) {
+            global $sqlErr;
+            $db = dbConn::getConnection();
+            if (!empty($db)) {
+                $tableName = get_called_class();
+                $sql = 'DELETE * FROM ' . $tableName . ' WHERE id = ' . $id;
+                try {
+                    $statement = $db->prepare($sql);
+                    $statement->execute();
+                    $result = htmlTags::changeRow('I just deleted record with id = ' . $this->id);
+                    return $result;
+                } catch (PDOException $e){
+                    $sqlErr .= htmlTags::changeRow('SQL query error: ' . $e->getMessage());
+                }
+            }
+        }
+    }
+?>
