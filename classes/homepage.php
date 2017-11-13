9d7585877492cba68eee1716e180fec4fb496c86
Dale <yl622@njit.edu>
2017-11-12 19:09:55 -0500

    Because of the objtoarray function, we don't need to consider too much when creating table.

--- classes/homepage.php
+++ classes/homepage.php
@@ -2,8 +2,10 @@
     class homepage extends page {
         public function get()
         {
-            $res = accounts::findAll();
-            $this->html .= table::createWholeTable($res);
+            $res1 = accounts::findAll();
+            $res2 = todos::findOne(3);
+            $this->html .= table::createTable($res1);
+            $this->html .= table::createTable($res2);
         }
     }
 ?>
--- classes/table.php
+++ classes/table.php
@@ -1,38 +1,23 @@
 <?php
     class table {
         static public function createTable($inputArray) {
-            if (arrayFunctions::countNumber($inputArray,0) != arrayFunctions::countNumber($inputArray,COUNT_RECURSIVE)){
-                $table = htmlTags::tableHead('displayTable');
-                foreach ($inputArray as $key => $line) {
-                    if ($key == 0) {
-                        $table .= htmlTags::tableLineStart();
-                        foreach ($line as $columns => $value) {
-                            $table .= htmlTags::tableTitle($columns);
-                        }
-                        $table .= htmlTags::tableLineEnd();
-                    }
+            $table = htmlTags::tableHead('displayTable');
+            foreach ($inputArray as $key => $line) {
+                if ($key == 0) {
                     $table .= htmlTags::tableLineStart();
                     foreach ($line as $columns => $value) {
-                        $table .= htmlTags::tableDetail($value);
+                        $table .= htmlTags::tableTitle($columns);
                     }
                     $table .= htmlTags::tableLineEnd();
                 }
-                $table .= htmlTags::tableEnd();
-                return $table;
-            } else {
-                $table = htmlTags::tableHead('displayTable');
                 $table .= htmlTags::tableLineStart();
-                foreach ($inputArray as $key => $value) {
-                    $table .= htmlTags::tableTitle($key);
-                }
-                $table .= htmlTags::tableLineEnd();
-                $table .= htmlTags::tableLineStart();
-                foreach ($inputArray as $key => $value) {
+                foreach ($line as $columns => $value) {
                     $table .= htmlTags::tableDetail($value);
                 }
                 $table .= htmlTags::tableLineEnd();
-                return $table;
             }
+            $table .= htmlTags::tableEnd();
+            return $table;
         }
     }
 ?>
