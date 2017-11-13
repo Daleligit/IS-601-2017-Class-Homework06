56771046599f8893a76013c242224c86de0b5f39
Dale <yl622@njit.edu>
2017-11-12 21:41:36 -0500

    Added a new function in form.php to create a form to let user to make a findOne method.

--- classes/form.php
+++ classes/form.php
@@ -1,12 +1,20 @@
 <?php
     class form {
-        static public function createSelectForm () {
+        static public function createTableSelectForm () {
             $form = '<form action="index.php?page=homepage" method="post">';
             $form .= '<select name="Selection"> ';
             $form .= '<option>accounts';
             $form .= '<option>todos';
             $form .= '</select>';
             $form .= '<input type="submit" value="Select">';
+            $form .= '</form>';
+            return $form;
+        }
+        static public function createFindIdForm () {
+            $form = '<form action="index.php?page=display" method="get">';
+            $form .= '<input type="text" name="id"> ';
+            $form .= '<input type="submit" value="id" name="submit">';
+            $form .= '</form>';
             return $form;
         }
     }
