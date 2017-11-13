4dc49e7e0468f18ebd435c14aa21a85799e0bc57
Dale <yl622@njit.edu>
2017-11-12 21:29:48 -0500

    Changed selection name in form.php

--- classes/form.php
+++ classes/form.php
@@ -2,7 +2,7 @@
     class form {
         static public function createSelectForm () {
             $form = '<form action="index.php?page=homepage" method="post">';
-            $form .= '<select name="products"> ';
+            $form .= '<select name="Selection"> ';
             $form .= '<option>accounts';
             $form .= '<option>todos';
             $form .= '</select>';
