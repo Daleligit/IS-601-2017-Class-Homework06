4c024e3b6770576857f5638a48c4e822e1490288
Dale <yl622@njit.edu>
2017-11-12 22:24:04 -0500

    Added functions in form.php to make forms to let user input information in display.php

--- classes/form.php
+++ classes/form.php
@@ -10,10 +10,42 @@
             $form .= '</form>';
             return $form;
         }
-        static public function createFindIdForm () {
-            $form = '<form action="index.php?page=display" method="get">';
-            $form .= '<input type="text" name="id"> ';
-            $form .= '<input type="submit" value="id" name="submit">';
+        static public function createFindAllForm ($tableName) {
+            $form = '<form action="index.php?page=display&table=' . $tableName . '&method=findAll" method="post">';
+            $form .= '<input type="submit" value="Display Whole Table" name="submit">';
+            $form .= '</form>';
+            return $form;
+        }
+        static public function createFindIdForm ($tableName) {
+            $form = '<form action="index.php?page=display&table=' . $tableName . '&method=findOne" method="post">';
+            $form .= '<input type="text" name="id">';
+            $form .= '<input type="submit" value="Display the line with this ID" name="submit">';
+            $form .= '</form>';
+            return $form;
+        }
+        static public function createDeleteForm ($tableName) {
+            $form = '<form action="index.php?page=display&table=' .$tableName . '&method=delete" method="post">';
+            $form .= '<input type="text" name="id">';
+            $form .= '<input type="submit" value="Delete the line with this ID" name="submit">';
+            $form .= '</form>';
+            return $form;
+        }
+        static public function createSaveForm ($tableName) {
+            $form = '<form action="index.php?page=display&table=' . $tableName . '&method=Save" method="post">';
+            switch ($tableName) {
+                case 'accounts';
+                    $res = accounts::findAll();
+                    break;
+                case 'todos';
+                    $res = todos::findAll();
+                    break;
+            }
+            $res = $res[0];
+            foreach ($res as $key=>$value) {
+                $form .= '<input type="text" name="' . $key . '">';
+                $form .= $key . '</br>';
+            }
+            $form .= '<input type="submit" value="Update/Insert the line with this ID" name="submit">';
             $form .= '</form>';
             return $form;
         }
