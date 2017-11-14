c9acede5b009fb7809f1b9af14d99d85eae7483c
Dale <yl622@njit.edu>
2017-11-14 13:32:38 -0500

    Made some improvements on grammar

--- classes/display.php
+++ classes/display.php
@@ -15,82 +15,8 @@
         public function post() {
             $tableName = $_GET['table'];
             $method = $_GET['method'];
-            switch ($method) {
-                case 'findAll';
-                    switch ($tableName) {
-                        case 'accounts';
-                            $this->html .= table::createTable(accounts::findAll());
-                            break;
-                        case 'todos';
-                            $this->html .= table::createTable(todos::findAll());
-                            break;
-                    }
-                    break;
-                case 'findOne';
-                    $id = $_POST['id'];
-                    if (!empty($id)) {
-                        switch ($tableName) {
-                            case 'accounts';
-                                $this->html .= table::createTable(accounts::findOne($id));
-                                break;
-                            case 'todos';
-                                $this->html .= table::createTable(todos::findOne($id));
-                                break;
-                        }
-                    } else {
-                        $this->html .= htmlTags::changeRow('Please input an ID');
-                    }
-                    break;
-                case 'delete';
-                    $id = $_POST['id'];
-                    if (!empty($id)) {
-                        switch ($tableName) {
-                            case 'accounts';
-                                $account = new account();
-                                $this->html .= $account->delete($id);
-                                break;
-                            case 'todos';
-                                $todo = new todo();
-                                $this->html .= $todo->delete($id);
-                                break;
-                        }
-                    } else {
-                        $this->html .= htmlTags::changeRow('Please input an ID');
-                    }
-                    break;
-                case 'save';
-                    $id = $_POST['id'];
-                    if (!empty($id)) {
-                        switch ($tableName) {
-                            case 'accounts';
-                                $account = new account();
-                                $account->id = $id;
-                                $account->email = $_POST['email'];
-                                $account->fname = $_POST['fname'];
-                                $account->lname = $_POST['lname'];
-                                $account->phone = $_POST['phone'];
-                                $account->birthday = $_POST['birthday'];
-                                $account->gender = $_POST['gender'];
-                                $account->password = $_POST['password'];
-                                $this->html .= $account->save();
-                                break;
-                            case 'todos';
-                                $todo = new todo();
-                                $todo->id = $id;
-                                $todo->owneremail = $_POST['owneremail'];
-                                $todo->ownerid = $_POST['ownerid'];
-                                $todo->createddate = $_POST['createddate'];
-                                $todo->duedate = $_POST['duedate'];
-                                $todo->message = $_POST['message'];
-                                $todo->isdone = $_POST['isdone'];
-                                $this->html .= $todo->save();
-                                break;
-                        }
-                    } else {
-                        $this->html .= htmlTags::changeRow('Please input an ID');
-                    }
-                    break;
-            }
+            $id = pageFunctions::getID($method);
+            $this->html .= pageFunctions::runMethod($method,$tableName,$id);
             $this->html .= htmlTags::turnPage('index.php?page=display&table=' . $tableName,'Back');
         }
     }
--- classes/pageFunctions.php
+++ classes/pageFunctions.php
@@ -17,5 +17,88 @@
         static public function changePage($linkUrl) {
             header('Location: ' . $linkUrl);
         }
+        static public function runMethod ($method,$tableName,$id) {
+            switch ($method) {
+                case 'findAll';
+                    switch ($tableName) {
+                        case 'accounts';
+                            $result = table::createTable(accounts::findAll());
+                            break;
+                        case 'todos';
+                            $result = table::createTable(todos::findAll());
+                            break;
+                    }
+                    break;
+                case 'findOne';
+                    if (!empty($id)) {
+                        switch ($tableName) {
+                            case 'accounts';
+                                $result = table::createTable(accounts::findOne($id));
+                                break;
+                            case 'todos';
+                                $result = table::createTable(todos::findOne($id));
+                                break;
+                        }
+                    } else {
+                        $result = htmlTags::changeRow('Please input an ID');
+                    }
+                    break;
+                case 'delete';
+                    if (!empty($id)) {
+                        switch ($tableName) {
+                            case 'accounts';
+                                $account = new account();
+                                $result = $account->delete($id);
+                                break;
+                            case 'todos';
+                                $todo = new todo();
+                                $result = $todo->delete($id);
+                                break;
+                        }
+                    } else {
+                        $result = htmlTags::changeRow('Please input an ID');
+                    }
+                    break;
+                case 'save';
+                    if (!empty($id)) {
+                        switch ($tableName) {
+                            case 'accounts';
+                                $account = new account();
+                                $account->id = $id;
+                                $account->email = $_POST['email'];
+                                $account->fname = $_POST['fname'];
+                                $account->lname = $_POST['lname'];
+                                $account->phone = $_POST['phone'];
+                                $account->birthday = $_POST['birthday'];
+                                $account->gender = $_POST['gender'];
+                                $account->password = $_POST['password'];
+                                $result = $account->save();
+                                break;
+                            case 'todos';
+                                $todo = new todo();
+                                $todo->id = $id;
+                                $todo->owneremail = $_POST['owneremail'];
+                                $todo->ownerid = $_POST['ownerid'];
+                                $todo->createddate = $_POST['createddate'];
+                                $todo->duedate = $_POST['duedate'];
+                                $todo->message = $_POST['message'];
+                                $todo->isdone = $_POST['isdone'];
+                                $result = $todo->save();
+                                break;
+                        }
+                    } else {
+                        $result = htmlTags::changeRow('Please input an ID');
+                    }
+                    break;
+            }
+            return $result;
+        }
+        static public function getID ($method) {
+            if ($method != 'findAll') {
+                return $_POST['id'];
+            } else {
+                return null;
+            }
+        }
     }
 ?>
